b80864b6f4adc40e9afc81d36831f28a21ec7c8b
sadlinqaq <2194738414@qq.com>
2026-04-20 18:02:54 +0800

    deploy website

--- astro.config.ts
+++ astro.config.ts
@@ -26,7 +26,7 @@ import config from './src/site.config.ts'
 export default defineConfig({
   // [Basic]
   site:'https://sadlinqaq.github.io',
-  base:'/',
+  base:'/AAA',
   // site: 'https://astro-pure.js.org',
   // Deploy to a sub path
   // https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
